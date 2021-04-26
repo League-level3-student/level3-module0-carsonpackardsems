e212e9c7ba4522dcd1bfac692f1f124526d6d640
student <student@ip-172-31-63-103.us-west-2.compute.internal>
2021-04-26 13:04:34 -0700

    commit and puuuuuuush

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -4,16 +4,42 @@ import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
-
+public static void main(String[] args) {
+	String [] allSortsOfNames = new String[11];
+	
+	allSortsOfNames[0] = "Matthew McCallister";
+	allSortsOfNames[1] = "Ryan Anders";
+	allSortsOfNames[2] = "Whitely Smith";
+	allSortsOfNames[3] = "Gabriella Di Angelo";
+	allSortsOfNames[4] = "Aiden Ramirez";
+	allSortsOfNames[5] = "Tis";
+	allSortsOfNames[6] = "Jimmy Ramirez";
+	allSortsOfNames[7] = "Python";
+	allSortsOfNames[8] = "Addilyn McCallister";
+	allSortsOfNames[9] = "Christy Hews";
+	allSortsOfNames[10] = "Python";
+	
+	printStrings(allSortsOfNames);
+	
+	reverseStrings(allSortsOfNames);
+	}
 
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-
+public static void printStrings (String [] Strings) {
+	for (int i = 0; i < Strings.length; i++) {
+		System.out.println(Strings[i]);
+	}
+}
 
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
-
+public static void reverseStrings (String [] Strings) {
+	for(int i = Strings.length; i > 1; i--) {
+		System.out.println(Strings[i]);
+	}
+}
 
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,16 +4,22 @@ public class _00_1D_Array_Methods {
     //1. Complete the method so that it returns the sum of all
     //   of the integers in the array being passed in
     public static int sumIntArray(int[] values) {
-for(int i = 0; i < values.length; i++)
-        return 0;
-return 0;
+    	int sumAddition = 0;
+for(int i = 0; i < values.length; i++) {
+	sumAddition = sumAddition + values[i];
+}
+        return sumAddition;
     }
 
     //2. Complete the method so that it returns the average of all
     //   of the integers in the array being passed in
     public static double averageIntArray(int[] values) {
-
-        return 0;
+    	double sumAdditionDivision = 0;
+for(int i = 0; i < values.length; i++) {
+	sumAdditionDivision = sumAdditionDivision + values[i];
+}
+sumAdditionDivision = sumAdditionDivision / values.length;
+        return sumAdditionDivision;
     }
 
 
@@ -21,15 +27,36 @@ return 0;
     //   array contains the value specified by the second parameter.
     //   It should otherwise return false.
     public static boolean containsIntValue(int[] array, int value) {
-
-        return false;
+    	boolean doesContainInt = false;
+for(int i = 0; i < array.length; i++) {
+	if(array[i] == value) {
+		doesContainInt = true;
+	}
+	else {
+		doesContainInt = false;
+	}
+}
+        return doesContainInt;
     }
 
     //4. Complete the method so that it returns the index of the,
     //   first instance that the specified value occurs in the array.
     //   If the array does not contain the specified value, it should return -1.
     public static int getIndex(int[] arr, int value) {
+    	int firstInstance = 0;
+    	boolean firstInstanceFound = false;
+for(int i = 0; i < arr.length; i++) {
+	if(firstInstanceFound == false) {
+	if(arr[i] == value) {
+		firstInstance = i;
+		firstInstanceFound = true;
+	}
+	else {
+		firstInstance = -1;
+	}
+}
+	
+}return firstInstance;
 
-        return 0;
     }
 }
